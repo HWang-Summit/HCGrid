b0c65b601f0eb2b13cde7050a46326062d068f00
HWang-Summit <h.wang.imu@gmail.com>
2021-07-18 12:11:14 +0800

    update information

--- HCGrid+/HCGrid.cpp
+++ HCGrid+/HCGrid.cpp
@@ -5,6 +5,33 @@
 // author                 :
 //
 // --------------------------------------------------------------------
+// Copyright (C) 2010+ by Hao Wang, Qi Luo
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// Note: Some HEALPix related helper functions and convolution algorithm 
+// code are adopted from (HEALPix C++) library (Copyright (C) 2003-2012
+//  Max-Planck-Society; author Martin Reinecke) and Cygrid software 
+// (Copyright (C) 2010+ by Benjamin Winkel, Lars Flöer & Daniel Lenz),
+// respectively.
+// 
+// For more information about HEALPix, see http://healpix.sourceforge.net
+// Healpix_cxx is being developed at the Max-Planck-Institut fuer Astrophysik
+// and financially supported by the Deutsches Zentrum fuer Luft- und Raumfahrt
+// (DLR).
+// 
+// For more information about Cygrid, see https://github.com/bwinkel/cygrid.
+// Cygrid was developed in the framework of the Effelsberg-Bonn H i Survey (EBHIS).
+// 
+// --------------------------------------------------------------------
 
 #include "HCGrid.h"
 
